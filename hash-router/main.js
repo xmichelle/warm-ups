b441e3f55721f0061a141d99fba5be675ed8659a
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-04 10:10:15 -0700

    Created and linked index.html and main.js files. #30

---
+++ hash-router/main.js
@@ -0,0 +1,13 @@
+// 
+// class HashRouter {
+//   constructor() {
+//     this.isListening = false
+//   }
+//   listen() {
+//     if (isListening === true) {
+//
+//     }
+//   }
+// }
+//
+// console.log(window.location.hash)
