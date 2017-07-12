1587bf7d6a4af30c390b76f09d3262a28599234b
xu.miche@gmail.com <xu.miche@gmail.com>
2017-07-12 09:55:28 -0700

    Created a class function that satisfied the exercise prompt. #39

--- linked-list/main.js
+++ linked-list/main.js
@@ -1,22 +1,22 @@
-// 
-// class LinkedList {
-//   constructor() {
-//     this.length = 0
-//     this.head = null
-//   }
-//   add(node) {
-//     if (this.head === null) {
-//       this.head = node
-//     }
-//     else {
-//       const last = this.head
-//       while (last.next !== null) {
-//         last = last.next
-//       }
-//       last.next = node
-//     }
-//     this.length++
-//   }
-// }
-//
-// const list = new LinkedList()
+
+class LinkedList {
+  constructor() {
+    this.length = 0
+    this.head = null
+  }
+  add(node) {
+    if (this.head === null) {
+      this.head = node
+    }
+    else {
+      const last = this.head
+      while (last.next !== null) {
+        last = last.next
+      }
+      last.next = node
+    }
+    this.length++
+  }
+}
+
+const list = new LinkedList()
