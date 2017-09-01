03ddbaa73b2384f144933cd0f5363adfe8173129
Michelle <michelle@Oranges-MacBook-Air.local>
2017-08-31 19:38:40 -0700

    Defined a function that satisfied the exercise prompt. #63

--- sum/main.js
+++ sum/main.js
@@ -1,1 +1,10 @@
+function getSum(a, b) {
+  let total = 0
+  const bigger = a > b ? a : b
+  const smaller = a > b ? b : a
 
+  for(let i = smaller; i <= bigger; i++) {
+    total += i
+  }
+  return total
+}
