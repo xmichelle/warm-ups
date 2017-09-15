5bacf2d35c43a5ec66d88a5eea04979725746909
xmichelle <xu.miche@gmail.com>
2017-09-14 20:21:47 -0700

    Defined a function that satisfies the exercise prompt. #81

---
+++ reverse/main.js
@@ -0,0 +1,11 @@
+function solution(str) {
+  const stringArray = str.split('')
+
+  let backwardArray = []
+
+  for (let i = stringArray.length - 1; i >= 0; i--) {
+    backwardArray.push(stringArray[i])
+  }
+
+  return backwardArray.join('')
+}
