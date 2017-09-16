e589f9ab277c9ace5ff33376d461f58ed3de34ff
xmichelle <xu.miche@gmail.com>
2017-09-15 19:51:36 -0700

    Defined a function that satisfied the exercise prompt with one line. #83

--- remove-anchor/main.js
+++ remove-anchor/main.js
@@ -1,5 +1,9 @@
-function removeUrlAnchor(url){
+function removeUrlAnchor(url) {
   const anchorSplit = url.split('#')
 
   return anchorSplit[0]
+}
+
+function removeUrlAnchor2(url) {
+  return url.split('#')[0]
 }
