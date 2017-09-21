ccb4021f968eae9f81301e6a7b10facb8193964e
xmichelle <xu.miche@gmail.com>
2017-09-21 00:52:50 -0700

    Defined another function that satisfied the exercise prompt using the map method. #89

--- kooka-counter/main.js
+++ kooka-counter/main.js
@@ -1,10 +1,30 @@
 function kookaCounter(laughing) {
   let count = 0
 
-  for(let i = 0; i < laughing.length; i += 2) {
-    if(laughing[i] !== laughing[i - 2]) {
+  for (let i = 0; i < laughing.length; i += 2) {
+    if (laughing[i] !== laughing[i - 2]) {
       count++
     }
   }
+  return count
+}
+
+// Another method using map
+
+function kookaCounter2(laughing) {
+  let count = 0
+  const laughingArray = [...laughing]
+
+  if (laughing === '') return 0
+
+  count++
+  laughingArray.map((cur, i, array) => {
+    if (cur === 'H' && array[i + 2] === 'h') {
+      count++
+    }
+    else if (cur === 'h' && array[i + 2] === 'H') {
+      count++
+    }
+  })
   return count
 }
