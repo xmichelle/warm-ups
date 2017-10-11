0b67ec582ba1849137adb7068bfc770b7797374e
xmichelle <xu.miche@gmail.com>
2017-10-10 23:42:57 -0700

    Refactored the code. #95

--- throttle/main.js
+++ throttle/main.js
@@ -1,7 +1,18 @@
 function throttle(func, wait) {
-  return window.setInterval(func, wait)
+  let waiting = false
+  return function() {
+    if (!waiting) {
+      func.call()
+      waiting = true
+      setTimeOut(() => {
+        waiting = false
+      }, wait)
+    }
+  }
 }
 
 const greeting = function() {
   console.log('hello world')
 }
+
+window.addEventListener('click', throttle(greeting, 5000))
