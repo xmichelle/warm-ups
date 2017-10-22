7de141acf860e2afee0db7860617a1e9625f8d83
xmichelle <xu.miche@gmail.com>
2017-10-22 14:53:07 -0700

    Satisfied the exercise prompt using a for loop. #99

--- factorial/main.js
+++ factorial/main.js
@@ -13,3 +13,19 @@ function factorial(n) {
   }
   return result
 }
+
+// Another way using a for loop
+
+function factorial2(n) {
+  if (n === 0 || n === 1) {
+    return 1
+  }
+  else if (n < 1) {
+    return 'Invalid value'
+  }
+
+  for (let i = n - 1; i >= 1; i--) {
+    n = n * i
+  }
+  return n
+}
