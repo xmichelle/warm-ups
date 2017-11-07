f34dfc4e269aaadff7397575542927d86871acde
xmichelle <xu.miche@gmail.com>
2017-11-06 22:59:46 -0800

    Improved the function. #103

--- fizzbuzz/main.js
+++ fizzbuzz/main.js
@@ -15,4 +15,27 @@ function fizzbuzz(nums) {
   }
 }
 
-fizzbuzz(100)
+// fizzbuzz(100)
+
+// Improved function with nesting
+
+function fizzbuzz2(nums) {
+  for (let i = 0; i < nums; i++) {
+    if (i % 3 === 0) {
+      if (i % 5 === 0) {
+        console.log('fizzbuzz')
+      }
+      else {
+        console.log('fizz')
+      }
+    }
+    else if (i % 5 === 0) {
+      console.log('buzz')
+    }
+    else {
+      console.log(i)
+    }
+  }
+}
+
+fizzbuzz2(100)
