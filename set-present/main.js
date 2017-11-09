029e0cf71dfd73cca2bee81643edef25c81dfb57
xmichelle <xu.miche@gmail.com>
2017-11-08 20:20:05 -0800

    Improved the function. #105

--- set-present/main.js
+++ set-present/main.js
@@ -1,8 +1,12 @@
-function numPresent(firstArray, secondArray) {
+function numPresent(arr, sub) {
   let count = 0
-  for (let i = 0; i < firstArray.length; i++) {
-    for (let j = 0; j < secondArray.length; j++) {
-      if (firstArray[i + j] === secondArray[j] && secondArray.length === j + 1) {
+  for (let i = 0; i < arr.length; i++) {
+    // Prevents the loop from continuing if it knows that the rest of the values in the first array won't match the second one.
+    if (i >= arr.length - (sub.length - 1)) {
+      return count
+    }
+    for (let j = 0; j < sub.length; j++) {
+      if (arr[i + j] === sub[j] && sub.length === j + 1) {
         count++
       }
     }
