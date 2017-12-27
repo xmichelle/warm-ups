3fac50dda5c97c677512600c38dafeb1f9f89110
xmichelle <xu.miche@gmail.com>
2017-12-26 22:13:15 -0800

    Defined a shorter function that satisfied the exercise prompt. #117

--- bounce-ball/main.js
+++ bounce-ball/main.js
@@ -10,3 +10,14 @@ function bouncingBall(initial, proportion) {
   }
   return bounces.length
 }
+
+// A shorter way of writing the function
+
+function bouncingBall2(initial, proportion) {
+  let count = 0
+
+  for (let i = initial; i > 1; i *= proportion) {
+    count++
+  }
+  return count
+}
