94f8788a1ec40a5dc7da676c04d5cb7654e90e35
xmichelle <xu.miche@gmail.com>
2018-01-03 16:53:08 -0800

    Added a comment about half life formula. #129

--- half-life/main.js
+++ half-life/main.js
@@ -2,3 +2,5 @@ function halfLife(quantityInitial, quantityRemaining, time) {
   const halfLife = time / ( Math.log2(quantityInitial/quantityRemaining) )
   return halfLife
 }
+
+// Half life formula: Final Amount = Initial Amount * (1/2)^(time/half-life)
